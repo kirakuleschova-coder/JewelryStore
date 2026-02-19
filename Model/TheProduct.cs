96427ea3702bb05e4ce71580ae13ebd8152f9632
st403 <st403@WS403-05>
2026-02-19 09:12:36 +0300

    Исправила decimal на int в классе TheProduct

--- Model/TheProduct.cs
+++ Model/TheProduct.cs
@@ -3,7 +3,7 @@
     public class TheProduct : EFModel
     {
         public string Title { get; set; } = string.Empty;
-        public decimal Price { get; set; }
+        public int Price { get; set; }
         public string Product { get; set; } = string.Empty;
         public int Test { get; set; }
         public int ProductionDate { get; set; }
