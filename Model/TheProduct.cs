c2fd690fa0e99ce0ff195f40a7bf921ac41db6f7
st403 <st403@WS403-05>
2026-04-23 08:18:37 +0300

    Добавила Range в продукте, для цены и пробы

--- Model/TheProduct.cs
+++ Model/TheProduct.cs
@@ -1,10 +1,17 @@
-﻿namespace JewelryStore.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace JewelryStore.Model
 {
     public class TheProduct : EFModel
     {
+        [Required(ErrorMessage = "Требуется название.")]
+        [JsonIgnore]
         public string Title { get; set; } = string.Empty;
+        [Range(1, 1000000, ErrorMessage = "Цена должна быть от 1 до 1000000")]
         public int Price { get; set; }
         public string Product { get; set; } = string.Empty;
+        [Range(1, 5, ErrorMessage = "Проба должна быть от 1 до 5")]
         public int Test { get; set; }
         public int ProductionDate { get; set; }
     }
